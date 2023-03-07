13d7161d5920c17969389b4325ce611b73b26fb0
Joyharsh <joyharsh13@gmail.com>
2023-03-07 16:49:22 +0530

    1111

--- Sources/Zen Beacon/Zen_Beacon.swift
+++ Sources/Zen Beacon/Zen_Beacon.swift
@@ -48,10 +48,10 @@ open class ZenBeaconScanner: NSObject, CLLocationManagerDelegate,UNUserNotificat
     open func Start_Scanning(AppID:String)
     {
         Str_APP_ID = AppID
-//        self.Get_Beacon_List(App_ID:Str_APP_ID)
+        self.Get_Beacon_List(App_ID:Str_APP_ID)
         
         
-        self.Ask_For_Location_Permission()
+//        self.Ask_For_Location_Permission()
 
     }
     
@@ -241,20 +241,20 @@ open class ZenBeaconScanner: NSObject, CLLocationManagerDelegate,UNUserNotificat
             //  Can we detect the distance of a beacon?
             if CLLocationManager.isRangingAvailable() {
                 
-//                for i in 0..<Arr_Beacon.count
-//                {
-//                    let Beacon_UUID = String(format: "%@", Arr_Beacon[i]["uuid"] as! String)
-//                    let Beacon_Identifier = String(format: "%@", Arr_Beacon[i]["location"] as! String)
-//                    let Beacon_Name = String(format: "%@", Arr_Beacon[i]["name"] as! String)
-//                    startScanning(uuid: UUID(uuidString: Beacon_UUID)!, major: 1, minor: 1, identifier: Beacon_Identifier, name: Beacon_Name)
-//                }
+                for i in 0..<Arr_Beacon.count
+                {
+                    let Beacon_UUID = String(format: "%@", Arr_Beacon[i]["uuid"] as! String)
+                    let Beacon_Identifier = String(format: "%@", Arr_Beacon[i]["location"] as! String)
+                    let Beacon_Name = String(format: "%@", Arr_Beacon[i]["name"] as! String)
+                    startScanning(uuid: UUID(uuidString: Beacon_UUID)!, major: 1, minor: 1, identifier: Beacon_Identifier, name: Beacon_Name)
+                }
                 
                 
                 
-                startScanning(uuid: UUID(uuidString: "5861636c-716e-2301-be01-6c731222f001")!, major: 1, minor: 1, identifier: "Apple Beacon", name: "Apple")
-                startScanning(uuid: UUID(uuidString: "5861636c-716e-2301-be01-6c731222f002")!, major: 1, minor: 1, identifier: "Radius Beacon", name: "Radius")
-                startScanning(uuid: UUID(uuidString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")!, major: 1, minor: 1, identifier: "Red Bear Beacon", name: "Red Bear")
-                startScanning(uuid: UUID(uuidString: "bf513d02-5ce1-411f-81f2-96d270f1cb2e")!, major: 1, minor: 1, identifier: "Estimote", name: "Estimote")
+//                startScanning(uuid: UUID(uuidString: "5861636c-716e-2301-be01-6c731222f001")!, major: 1, minor: 1, identifier: "Apple Beacon", name: "Apple")
+//                startScanning(uuid: UUID(uuidString: "5861636c-716e-2301-be01-6c731222f002")!, major: 1, minor: 1, identifier: "Radius Beacon", name: "Radius")
+//                startScanning(uuid: UUID(uuidString: "2F234454-CF6D-4A0F-ADF2-F4911BA9FFA6")!, major: 1, minor: 1, identifier: "Red Bear Beacon", name: "Red Bear")
+//                startScanning(uuid: UUID(uuidString: "bf513d02-5ce1-411f-81f2-96d270f1cb2e")!, major: 1, minor: 1, identifier: "Estimote", name: "Estimote")
 //                startScanning(uuid: UUID(uuidString: "bf513d02-5ce1-411f-81f2-96d270f1cb2e")!, major: 1, minor: 1, identifier: "Estimote", name: "Estimote")
             }
         }
