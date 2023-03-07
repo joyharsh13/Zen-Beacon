e1a4f62894817b7eaff545a654c751b6566b3468
Joyharsh <joyharsh13@gmail.com>
2023-03-07 12:43:24 +0530

    1

--- Sources/Zen Beacon/Zen_Beacon.swift
+++ Sources/Zen Beacon/Zen_Beacon.swift
@@ -92,12 +92,12 @@ open class ZenBeaconScanner: NSObject, CLLocationManagerDelegate,UNUserNotificat
                             }
                             else
                             {
-                                self.Open_Alert(Title: "Alert...!!", Message: dataDict["responseMsg"] as! String)
+                                
                             }
                         }
                         else
                         {
-                            
+                            self.Open_Alert(Title: "Alert...!!", Message: dataDict["responseMsg"] as! String)
                         }
                     }
                     else
