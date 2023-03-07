e593d23f7cceb532142c65964b1e43d9070c6014
Joyharsh <joyharsh13@gmail.com>
2023-03-07 17:39:25 +0530

    qwerty

--- Sources/Zen Beacon/Zen_Beacon.swift
+++ Sources/Zen Beacon/Zen_Beacon.swift
@@ -581,8 +581,16 @@ open class ZenBeaconScanner: NSObject, CLLocationManagerDelegate,UNUserNotificat
             
             if let Beacon_UUID = userInfo["Beacon_UUID"] as? String
             {
+                let state: UIApplication.State = UIApplication.shared.applicationState
                 
-                self.Get_Beacon_Advertise_Data(Beacon_UUID: Beacon_UUID, is_from_Notification: true)
+                if state != .active
+                {
+                    self.Get_Beacon_Advertise_Data(Beacon_UUID: Beacon_UUID, is_from_Notification: true)
+                }
+                else
+                {
+                    self.Get_Beacon_Advertise_Data(Beacon_UUID: Beacon_UUID, is_from_Notification: false)
+                }
                 
 //                let storyboard = UIStoryboard(name: "Main", bundle: nil)
 //
